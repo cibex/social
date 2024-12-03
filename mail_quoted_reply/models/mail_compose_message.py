2e1ab8b5b9695f2a63e50f454b51cdfb778e79e3
Christoph Abenthung <christoph.abenthung@cibex.net>
2024-12-03 10:31:37 +0100

    [IMP]mail_quoted_reply: Separate reply body
    For long replies the message composer gets extremely laggy and slow. To prevent that the reply body is stored in a separate field.

--- mail_quoted_reply/models/mail_compose_message.py
+++ mail_quoted_reply/models/mail_compose_message.py
@@ -1,18 +1,51 @@
 from markupsafe import Markup
 
-from odoo import api, models, tools
+from odoo import api, fields, models, tools
 
 
 class MailComposeMessage(models.TransientModel):
     _inherit = "mail.compose.message"
+
+    is_reply_readonly = fields.Boolean(default=True, string="Reply Readonly")
+    reply_body = fields.Html(default="", string="Reply body")
+    is_separate_body = fields.Boolean(compute="_compute_is_separate_body")
 
     @api.onchange("template_id")
     def _onchange_template_id_wrapper(self):
         super()._onchange_template_id_wrapper()
         context = self._context
         if "is_quoted_reply" in context.keys() and context["is_quoted_reply"]:
-            self.body += Markup(context["quote_body"])
+            if self.is_separate_body:
+                self.reply_body = context["quote_body"]
+            else:
+                self.body += Markup(context["quote_body"])
         return
+
+    @api.onchange("is_reply_readonly")
+    def _onchange_is_reply_readonly(self):
+        if self.reply_body:
+            self.reply_body = Markup(self.reply_body)
+
+    @api.depends("reply_body")
+    def _compute_is_separate_body(self):
+        if self._context.get("is_quoted_reply", False):
+            parameter_string = (
+                self.env["ir.config_parameter"]
+                .sudo()
+                .get_param("mail_quoted_reply.separate_reply_body", "")
+            )
+            self.is_separate_body = parameter_string.lower() not in ["", "false", "0"]
+        else:
+            self.is_separate_body = False
+
+    def get_mail_values(self, res_ids):
+        results = super(MailComposeMessage, self).get_mail_values(res_ids)
+        if self.is_separate_body and self.reply_body:
+            for res_id in res_ids:
+                values = results.get(res_id)
+                reply_body = Markup(self.reply_body)
+                values.update({"body": values.get("body") + reply_body})
+        return results
 
     @api.model
     def get_record_data(self, values):
